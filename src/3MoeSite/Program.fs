4970d1802e4cbe47274d9b17c5187a8fd1870a06
Aimdrol <accounts_aimdrol@hotmail.com>
2019-01-01 01:27:59 +0100

    Horizontally centered content

--- src/3MoeSite/Program.fs
+++ src/3MoeSite/Program.fs
@@ -35,15 +35,17 @@ module Views =
                        _href "/main.css" ]
             ]
             header [] [ navigation () ]
-            body [] content
+            main [] [
+                div [ _class "contentBox" ] content
+            ]
             footer [] [
-                br []
-                hr []
-                p  [ _class "footerText" ] [
-                    a  [_href "http://forum.worldoftanks.eu/index.php?/topic/524379-marks-of-excellence-data-a-new-metric-has-been-born-190117-dank-marks-can-melt-steel-beams/" ] [ encodedText "WoT forum thread" ]
-                    encodedText " | "
-                    a [ _href "http://forum.wotlabs.net/index.php?/topic/22352-marks-of-excellence-data-a-new-metric-has-been-born-251216-last-christmas-i-gave-you-my-marks/" ] [ encodedText "Wotlabs forum thread" ]
-                    encodedText " | Created by Aim_Drol and pun_xD 2016 - 2019"
+                div [ _class "footerBox" ] [
+                    p  [ _class "footerText" ] [
+                        a  [_href "http://forum.worldoftanks.eu/index.php?/topic/524379-marks-of-excellence-data-a-new-metric-has-been-born-190117-dank-marks-can-melt-steel-beams/" ] [ encodedText "WoT forum thread" ]
+                        encodedText " | "
+                        a [ _href "http://forum.wotlabs.net/index.php?/topic/22352-marks-of-excellence-data-a-new-metric-has-been-born-251216-last-christmas-i-gave-you-my-marks/" ] [ encodedText "Wotlabs forum thread" ]
+                        encodedText " | Created by Aim_Drol and pun_xD 2016 - 2019"
+                    ]
                 ]
             ]
         ]
--- src/3MoeSite/View/Navigation.fs
+++ src/3MoeSite/View/Navigation.fs
@@ -4,15 +4,17 @@ open Giraffe.GiraffeViewEngine
 
 let navigation () =
     [
-        ("/", "3 Moe Data")
-        ("/stats/distribution", "Mark distribution")
-        ("/marks", "Marks")
-        ("/tanks", "Tanks")
-        ("/players", "players")
-        ("/clans", "Clans")
-        ("/nations", "Nations")
-        ("/vehicle_tyles", "Vehicle types")
-        ("/tiers", "Tiers")
-    ] 
-    |> List.map (fun (url, text) -> li [ _class "headerButton" ] [ a [ _href url ] [ encodedText text ] ] )
-    |> ul [ _class "headerNavigationBar" ]
+        ([
+            ("/", "3 Moe Data")
+            ("/stats/distribution", "Mark distribution")
+            ("/marks", "Marks")
+            ("/tanks", "Tanks")
+            ("/players", "players")
+            ("/clans", "Clans")
+            ("/nations", "Nations")
+            ("/vehicle_tyles", "Vehicle types")
+            ("/tiers", "Tiers")
+        ] 
+        |> List.map (fun (url, text) -> li [ _class "headerButton" ] [ a [ _href url ] [ encodedText text ] ] )
+        |> ul [ _class "headerNavigationBar" ])
+    ] |> div [ _class "headerBox" ]
